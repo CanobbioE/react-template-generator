a207475c5d1ab416c6cf486a94504c4b2d4b02cd
Edoardo <canobbio.e@gmail.com>
2020-02-26 11:46:51 +0100

    Added fonts and styles to template

--- cmd/rtg/templates.go
+++ cmd/rtg/templates.go
@@ -3,6 +3,7 @@ package main
 const componentTemplate = `
 import React from "react";
 import { View, Text} from "react-native";
+import styles from "./{{.Name}}Styles"; 
 
 interface {{.Name}}Props {
 
@@ -21,7 +22,7 @@ export default {{.Name}};
 
 const styleTemplate = `
 import { StyleSheet } from "react-native";
-import { fonts } from "../../Styles";
+import { fonts, color } from "../../Styles";
 
 const styles = StyleSheet.create({
   
